40b669cf59b736a7ffe4f4cbb4220ac4a601988f
dima <martin.1.ma@nokia-sbell.com>
2024-03-08 16:02:00 +0000

    Add python lsp support

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,6 +4,37 @@ local capabilities = base.capabilities
 
 local lspconfig = require("lspconfig")
 
+lspconfig.pylsp.setup {
+  on_attach = function (client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  settings = {
+    pylsp = {
+      plugins = {
+        -- formatter options
+        black = { enabled = false },
+        autopep8 = { enabled = true, maxLineLength = 120 },
+        yapf = { enabled = false },
+        -- linter options
+        pylint = { enabled = true, executable = "pylint", args = "--max-line-length 120" },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = { enabled = true },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        pyls_isort = { enabled = true },
+      },
+    },
+  },
+  flags = {
+    debounce_text_changes = 200,
+  },
+  capabilities = capabilities,
+}
+
 lspconfig.clangd.setup {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
